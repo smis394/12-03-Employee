c6682386efabb7f67166707f72ce2baa40e42d4a
smis394 <shambhavimishra46@gmail.com>
2014-12-03 21:16:33 -0700

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,1 +1,54 @@
 // placeholder for your Employee class
+public class Employee
+{
+  String name;
+  double hourlyWage;
+  int hoursWorked;
+  public Employee(String personName, double wage, int hours)
+  {
+    name=personName;
+    hourlyWage=wage;
+    hoursWorked=hours;
+  }
+  public String getName()
+  {
+    return name;
+  }
+  public double getWage()
+  {
+    return hourlyWage;
+  }
+  public int getHours()
+  {
+    return hoursWorked;
+  }
+  public double getWeeklySalary()
+  {
+    double weeklySalary=hoursWorked*hourlyWage;
+    return weeklySalary;
+  }
+  public double getYearlySalary()
+  {
+    return getWeeklySalary()*52;
+  }
+  public String toString()
+  {
+    return "name: "+name+" wage: $"+hourlyWage+" hours: "+hoursWorked;
+  }
+  public void setName(String newName)
+  {
+    name=newName;
+  }
+  public void setWage(double newWage)
+  {
+    hourlyWage=newWage;
+  }
+  public void setHours(int h)
+  {
+    hoursWorked=h;
+  }
+  public void giveRaise()
+  {
+    hourlyWage+=1;
+  }
+}
